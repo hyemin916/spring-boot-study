daef15917f9140ace621111d748f9d08086c7aac
박혜민 <hyemin@ktown4u.com>
2023-01-15 20:00:01 +0900

    controller

---
+++ src/test/java/springboot/web/HelloResponseDtoTest.java
@@ -0,0 +1,21 @@
+package springboot.web;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloResponseDtoTest {
+
+    @Test
+    public void 롬복_기능_테스트() {
+        String name = "test";
+        int amount = 1000;
+
+        final HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
