784bf9e8543075124d9c9e2c97e26989dacd42cf
박혜민 <hyemin@ktown4u.com>
2023-01-20 23:02:18 +0900

    test

---
+++ src/test/java/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,41 @@
+package springboot.domain.posts;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @AfterEach
+    void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    void 게시글저장_불러오기() {
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("hyemin@gamil.com")
+                .build());
+
+        final List<Posts> postsList = postsRepository.findAll();
+
+        final Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+}
