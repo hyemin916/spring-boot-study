36f3d56c0b4494948dbfaa039e59e5d1188ae5f5
박혜민 <hyemin@ktown4u.com>
2023-01-23 21:48:02 +0900

    test

--- src/test/java/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/springboot/domain/posts/PostsRepositoryTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -38,4 +39,21 @@ class PostsRepositoryTest {
         assertThat(posts.getContent()).isEqualTo(content);
     }
 
+    @Test
+    void BaseTimeEntity_등록() {
+        final LocalDateTime now = LocalDateTime.of(2023, 1, 23, 0, 0, 0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        final List<Posts> all = postsRepository.findAll();
+
+        final Posts posts = all.get(0);
+        System.out.println(">>>>>>>>>>> createdDate=" + posts.getCreatedDate() + ", modifiedDate=" + posts.getModifiedDate());
+
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
+    }
 }
