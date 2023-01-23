5ef02c4ee8a003632b05ad921ae84fd06cce4e7d
박혜민 <hyemin@ktown4u.com>
2023-01-23 19:45:03 +0900

    fill classes

--- src/main/java/springboot/service/post/PostsService.java
+++ src/main/java/springboot/service/post/PostsService.java
@@ -1,4 +1,18 @@
 package springboot.service.post;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import springboot.domain.posts.PostsRepository;
+import springboot.web.dto.PostSaveRequestDto;
+
+@RequiredArgsConstructor
+@Service
 public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(final PostSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
 }
--- src/main/java/springboot/web/dto/PostSaveRequestDto.java
+++ src/main/java/springboot/web/dto/PostSaveRequestDto.java
@@ -1,4 +1,25 @@
 package springboot.web.dto;
 
+import lombok.Builder;
+import springboot.domain.posts.Posts;
+
 public class PostSaveRequestDto {
+    private String title;
+    private String content;
+    private String author;
+
+    @Builder
+    public PostSaveRequestDto(final String title, final String content, final String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public Posts toEntity() {
+        return Posts.builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .build();
+    }
 }
