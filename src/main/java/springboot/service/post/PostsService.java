b2c5a88bb3d227b8a268106068917909239dc0c8
박혜민 <hyemin@ktown4u.com>
2023-01-23 20:27:07 +0900

    service

--- src/main/java/springboot/service/post/PostsService.java
+++ src/main/java/springboot/service/post/PostsService.java
@@ -3,7 +3,9 @@ package springboot.service.post;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import springboot.domain.posts.Posts;
 import springboot.domain.posts.PostsRepository;
+import springboot.web.dto.PostsResponseDto;
 import springboot.web.dto.PostsSaveRequestDto;
 
 @RequiredArgsConstructor
@@ -12,7 +14,20 @@ public class PostsService {
     private final PostsRepository postsRepository;
 
     @Transactional
+    public Long update(final Long id, final PostsUpdateRequestDto requestDto) {
+        final Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+    }
+
+    @Transactional
     public Long save(final PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    public PostsResponseDto findById(final Long id) {
+        final Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(posts);
     }
 }
